d0c2f1c5844c4eddd8a2734eba26769f1fbd2d02
Frank Natividad <contact@franknatividad.com>
2012-12-09 05:30:09 -0700

    Adding Poisson Distrubution Formula function

---
+++ src/Poisson.cpp
@@ -0,0 +1,26 @@
+double poissonFormula(double k){
+ 
+  double e=2.71828;
+  double lambda=0.5;
+
+  //calculate lambda^k
+  int power=2;
+  double a=lambda;
+  double fa=lambda;
+  while(power <= k){
+    fa=fa*a;
+    power++;
+  }
+
+  //calculate k!  
+  double c=k-1;
+  double fc=k;
+  while(c!=0){
+    fc=fc*c;
+    c--;
+  }
+
+  double answer=(fa*0.60653)/fc;
+
+  return answer;
+}//end formula
