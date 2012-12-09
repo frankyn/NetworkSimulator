80a7a21bfaeb3d80c5274b1942e349ab24f0472d
Frank Natividad <contact@franknatividad.com>
2012-12-09 09:47:01 -0700

    Updating makefile for new custom container classes

--- src/Queue.h
+++ src/Queue.h
@@ -1,25 +1,36 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
 #include <iostream>
 #include <string>
 #include "Chain.h"
 
-#ifndef QUEUE_H
-#define QUEUE_H
-
-
 /*to make a queue on the main program, you just need construct the queue by typing Queue<"type"> "name of Queue" and call 
 the functions as you need them.*/ 
 template<typename E>
-class Queue : private Chain<E>
+class Queue: private Chain<E>
 {
       public:
-            Queue ( );
-            ~Queue ( );
-
-            void pushNode(E &data);
-            bool popNode(E &data);
-            bool emptyQueue();
-            void printQueue();
-       
+             void pushNode(E &data)
+             {
+                  insertAtBack(data);
+             }
+             
+             bool popNode(E &data)
+             {
+                  return removeFirst(data);
+             }
+             
+             bool emptyQueue()
+             {
+                  return this->isEmpty();
+             }
+             
+             void printQueue()
+             {
+                  this->print();
+             }
+             
 };//end class
 
 #endif
